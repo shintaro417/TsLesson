e2fcd0b8c00f05a224ed17f25cf5b8675e2a160a
shintaro417 <shintaro11417@gmail.com>
2021-03-02 22:09:12 +0900

    document commit

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
 const ts:string = "typescript";
 console.log(ts);
+
+// TypeScriptを使うと関数の使い方が誰がみても分かる→ドキュメントとして使える。(分かりやすくコードを書ける。)
+// JavaScriptだとコメントで説明するがコメントでは信頼性に欠ける。
+function add(a: number,b: number): number{
+    return a + b;
+}
