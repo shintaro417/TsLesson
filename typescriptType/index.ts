62d78bb5df205f2a5ea9cd1203883f10264de67a
shintaro417 <shintaro11417@gmail.com>
2021-03-03 20:35:38 +0900

    ts

---
+++ typescriptType/index.ts
@@ -0,0 +1,22 @@
+let hasValue: boolean = true;
+//numberは浮動小数点
+let count:number = 10;
+let float:number = 3.14;
+let minus:number = -10;
+
+//シングル、ダブル、バッククォート対応
+let single:string = 'hello';
+
+const person:{
+    name: string,
+    age: number
+} = {
+    name:'Jack',
+    age:21
+}
+
+const fruits: string[] = ['Apple','Banana','Grape'];
+const sample: any[] = ['a',1,'d'];
+
+//タプル
+const book:[string,number,boolean] = ['business',1500,false];
