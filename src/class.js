60fa54d1e84741749f4f91866ccda91f2ff92062
shintaro417 <shintaro11417@gmail.com>
2021-03-15 19:30:44 +0900

    commit

--- src/class.js
+++ src/class.js
@@ -1,13 +1,22 @@
 "use strict";
 var Person = /** @class */ (function () {
-    function Person(initName) {
+    function Person(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
     }
+    Person.prototype.incrementAge = function () {
+        this.age += 1;
+    };
+    //スコープ外でも値を保持し続けることができる。
     Person.prototype.greeting = function () {
-        console.log('Hello My name is ${this.name}');
+        console.log("Hello My name is " + this.name + ". I am " + this.age + " years old");
     };
     return Person;
 }());
-var quil = new Person('Quill');
-console.log(quil);
-quil.greeting();
+var person2 = Person;
+var quil = new Person('Quile', 32);
+var anotherQuil = {
+    name: 'anotherQuil',
+    anotherGreeting: quil.greeting
+};
+anotherQuil.anotherGreeting;
--- src/class.ts
+++ src/class.ts
@@ -1,19 +1,24 @@
 class Person{
     //フィールド
-    name:string;
-    constructor(initName:string){
+    public name:string;
+    private age:number;
+    constructor(initName:string,initAge:number){
         this.name = initName;
+        this.age = initAge;
+    }
+
+    private incrementAge(){
+        this.age += 1;
     }
 
     //スコープ外でも値を保持し続けることができる。
     greeting(this:Person){
-        console.log(`Hello My name is ${this.name}`);
+        console.log(`Hello My name is ${this.name}. I am ${this.age} years old`);
     }
 }
 
-const quil = new Person('Quill');
-console.log(quil);
-quil.greeting();
+let person2 = Person;
+const quil = new Person('Quile',32);
 
 const anotherQuil = {
     name:'anotherQuil',
