1ece6ca73fe6774b6727924530665e22f752d5e4
shintaro417 <shintaro11417@gmail.com>
2021-03-17 16:09:58 +0900

    commit

--- src/interface.ts
+++ src/interface.ts
@@ -1,11 +1,15 @@
 interface Human {
     name:String;
     age:number;
+    //greeting:(message:string) => void;
+    //オブジェクト内でのみ使える
+    greeting(message:string):void;
 } 
 
 const human:Human= {
     name: 'Quil',
-    age: 38
+    age: 38,
+    greeting(message:string){
+        console.log(message);
+    }
 }
-
-let developer:Human;
