1d4568122205a0d33af756add036407450320666
shintaro417 <shintaro11417@gmail.com>
2021-04-02 16:40:24 +0900

    デコレータ

--- src/decorator.js
+++ src/decorator.js
@@ -1,12 +1,56 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logging(target) {
-    console.log('Logging..');
+function Logging(message) {
+    return function (target) {
+        console.log('Logging..');
+        console.log(message);
+    };
+}
+// function Logging(target:Function){
+//     console.log('Logging..');
+// }
+function Component(template, selector) {
+    return function (constructor) {
+        return /** @class */ (function (_super) {
+            __extends(class_1, _super);
+            function class_1() {
+                var args = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    args[_i] = arguments[_i];
+                }
+                var _this = _super.apply(this, args) || this;
+                console.log('Component');
+                var mountedElement = document.querySelector(selector);
+                var instance = new constructor();
+                if (mountedElement) {
+                    mountedElement.innerHTML = template;
+                    mountedElement.querySelector('h1').textContent = instance.name;
+                }
+                return _this;
+            }
+            return class_1;
+        }(constructor));
+    };
 }
 var User = /** @class */ (function () {
     function User() {
@@ -14,7 +58,9 @@ var User = /** @class */ (function () {
         console.log('User was created');
     }
     User = __decorate([
-        Logging
+        Component('<h1>{{ name }}</h1>', '#app') //Userのnameに対応するようにする。index.htmlのid = appに代入する
+        ,
+        Logging('Loggin User')
     ], User);
     return User;
 }());
--- src/decorator.ts
+++ src/decorator.ts
@@ -1,8 +1,34 @@
-function Logging(target:Function){
-    console.log('Logging..');
+function Logging(message:string){
+    return function (target:Function){
+        console.log('Logging..');
+        console.log(message);
+    }
 }
 
-@Logging
+// function Logging(target:Function){
+//     console.log('Logging..');
+// }
+
+function Component(template:string,selector:string){
+    return function<T extends {new(...args:any[]): {name :string}} >(constructor:T){ //constructorをインスタンス化できるようにする。
+
+        return class extends constructor{
+            constructor(...args:any[]){
+                super(...args);
+                console.log('Component');
+                const mountedElement = document.querySelector(selector);
+                const instance = new constructor();
+                if(mountedElement){
+                    mountedElement.innerHTML = template;
+                    mountedElement.querySelector('h1')!.textContent = instance.name;
+                }
+            }
+        };
+    }
+}
+
+@Component('<h1>{{ name }}</h1>','#app') //Userのnameに対応するようにする。index.htmlのid = appに代入する
+@Logging('Loggin User')
 class User{
     name = 'aaa';
     constructor(){
