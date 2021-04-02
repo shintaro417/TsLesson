e41af94188911f87c5d762d40b5f1a721498f122
shintaro417 <shintaro11417@gmail.com>
2021-04-02 14:14:54 +0900

    クラスに対してジェネリクス

--- src/addvanced.js
+++ src/addvanced.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a, _b, _c;
 var quil = {
     name: 'Quil',
     role: 'front-end',
@@ -10,8 +11,10 @@ function toUpperCase(x) {
     if (typeof x === 'string') {
         return x.toUpperCase();
     }
-    return '';
+    return x;
 }
+//オーバーロードの全ての関数に対応した関数を入れなければならない。
+var upperHello = function (x) { return 0; };
 function describeProfile(nomadoWorker) {
     console.log(nomadoWorker.name);
     //in演算子:特定のオブジェクト型を特定する時に有用
@@ -24,6 +27,8 @@ function describeProfile(nomadoWorker) {
 }
 var Dog = /** @class */ (function () {
     function Dog() {
+        //タグつけ
+        this.kind = 'dog';
     }
     Dog.prototype.bark = function () {
         console.log('bow-bow');
@@ -32,6 +37,7 @@ var Dog = /** @class */ (function () {
 }());
 var Bird = /** @class */ (function () {
     function Bird() {
+        this.kind = 'bird';
     }
     Bird.prototype.bark = function () {
         console.log('tweet');
@@ -43,8 +49,45 @@ var Bird = /** @class */ (function () {
 }());
 function havPet(pet) {
     pet.bark();
-    //instanceof:変数の型を絞り込む
+    switch (pet.kind) {
+        case 'bird':
+            pet.fly;
+            break;
+        case 'dog':
+            pet.bark;
+            break;
+    }
+    //instanceof:変数の型を絞り込む。interfaceは使えない
     if (pet instanceof Bird) {
         pet.fly();
     }
 }
+//HTMLElementは抽象的だから手動で型を変える必要がある。->型アサーションか後に asをつける(Reactを使わない人向け)
+// const input = <HTMLInputElement>document.getElementById('input');
+// const input2 = document.getElementById('input') as HTMLInputElement
+document.getElementById('input').value = 'initial input';
+//None-null assertion operater->nullではないことを確定させる。->!を使う
+var input = document.getElementById('input');
+var designer = {
+    name: 'Quil',
+    role: 'web',
+};
+console.log(designer.role);
+var downLoadedData = {
+    id: 1
+};
+//Optional Chaining->undefinedであった場合にエラーを防ぐ処理
+console.log((_b = (_a = downLoadedData.user) === null || _a === void 0 ? void 0 : _a.name) === null || _b === void 0 ? void 0 : _b.firstName);
+//Nullish Coalescing->undefined/nullかそれ以外かで処理を分岐させる。(空文字 0はtrue扱い->or演算子とは違う)
+var userData = (_c = downLoadedData.user) !== null && _c !== void 0 ? _c : 'no-user';
+//enum型と数字は互換性がある。
+//関数型では代入先の関数の引数によっては引数が無視されることもある。
+//Classではprivate/protected修飾子がある場合は互換性がなくなる。
+var Color;
+(function (Color) {
+    Color[Color["RED"] = 0] = "RED";
+    Color[Color["BLUE"] = 1] = "BLUE";
+})(Color || (Color = {}));
+var target = Color.RED;
+var source = 100;
+target = source;
---
+++ src/generics.js
@@ -0,0 +1,26 @@
+"use strict";
+function copy(value, key) {
+    return value;
+}
+console.log(copy({ name: 'Quil', age: 38 }, 'age'));
+var LightDatabase = /** @class */ (function () {
+    function LightDatabase() {
+        this.data = [];
+    }
+    LightDatabase.prototype.add = function (item) {
+        this.data.push(item);
+    };
+    LightDatabase.prototype.remove = function (item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    LightDatabase.prototype.get = function () {
+        return this.data;
+    };
+    return LightDatabase;
+}());
+var stringLightDatabase = new LightDatabase();
+stringLightDatabase.add('apple');
+stringLightDatabase.add('banana');
+stringLightDatabase.add('grape');
+stringLightDatabase.remove('grape');
+console.log(stringLightDatabase.get());
