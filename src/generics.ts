5a68844e9c2c59e2ed6f4e64bf2cd1e8e9a511a9
shintaro417 <shintaro11417@gmail.com>
2021-04-02 14:07:00 +0900

    keyof演算子

--- src/generics.ts
+++ src/generics.ts
@@ -1,5 +1,5 @@
-function copy<T>(value:T):T{
+function copy<T extends {name :string},U extends keyof T>(value:T,key:U):T{
     return value;
 }
 
-console.log(copy<string>('hello'));
+console.log(copy({name:'Quil',age:38},'age'));
