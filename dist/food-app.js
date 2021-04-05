399424ee4b479799494a947d77486b2de5c9abcc
shintaro417 <shintaro11417@gmail.com>
2021-04-05 11:03:25 +0900

    commit

--- dist/food-app.js
+++ dist/food-app.js
@@ -3,7 +3,7 @@ var Score = /** @class */ (function () {
     }
     Object.defineProperty(Score.prototype, "totalScore", {
         get: function () {
-            var foods = new Foods();
+            var foods = Foods.getInstance();
             return foods.activeElementsScore.reduce(function (total, score) { return total + score; }, 0);
         },
         enumerable: false,
@@ -11,6 +11,12 @@ var Score = /** @class */ (function () {
     });
     Score.prototype.render = function () {
         document.querySelector('.score__number').textContent = String(this.totalScore);
+    };
+    Score.getInstance = function () {
+        if (!Score.instance) {
+            Score.instance = new Score();
+        }
+        return Score.instance;
     };
     return Score;
 }());
@@ -22,14 +28,14 @@ var Food = /** @class */ (function () {
     }
     Food.prototype.clickEventHandler = function () {
         this.element.classList.toggle('food--active'); //指定したクラスがあった場合はクラスを付け足す。
-        var score = new Score();
+        var score = Score.getInstance();
         score.render();
     };
     return Food;
 }());
 //Foodクラスの一覧
 var Foods = /** @class */ (function () {
-    //各要素を取得する。
+    //各要素を取得する。newをクラス内でしか使用できなくする。
     function Foods() {
         this.elements = document.querySelectorAll('.food'); //クラスがfoodの要素を全て取得する。ジェネリクスで要素を指定できる。
         this._activeElements = [];
@@ -72,6 +78,13 @@ var Foods = /** @class */ (function () {
         enumerable: false,
         configurable: true
     });
+    //インスタンスの取得
+    Foods.getInstance = function () {
+        if (!Foods.instance) {
+            Foods.instance = new Foods();
+        }
+        return Foods.instance;
+    };
     return Foods;
 }());
-var foods = new Foods();
+var foods = Foods.getInstance();
--- dist/food-app.ts
+++ dist/food-app.ts
@@ -1,25 +1,52 @@
-class Score{
+interface Scoreable{
+    readonly totalScore:number;
+    render():void;
+}
+
+interface Foodable{
+    element:HTMLDivElement;
+    clickEventHandler():void;
+}
+
+interface Foodsable{
+    elements:NodeListOf<HTMLDivElement>;
+    readonly activeElements:HTMLDivElement[];
+    readonly activeElementsScore:number[];
+}
+
+class Score implements Scoreable{
+    private static instance:Score;
     get totalScore(){
-        const foods = new Foods();
+        const foods = Foods.getInstance();
         return foods.activeElementsScore.reduce((total,score) => total + score, 0);
     }
     render(){
         document.querySelector('.score__number')!.textContent = String(this.totalScore);
     }
+    static getInstance(){
+        if(!Score.instance){
+            Score.instance = new Score();
+        }
+        return Score.instance
+    }
+
+    private constructor(){}
 }
 //Foodクラス
-class Food{
+class Food implements Foodable{
     constructor(public element:HTMLDivElement){
         element.addEventListener('click',this.clickEventHandler.bind(this)); //bindを使うことでthisを明示することができる。
     }
     clickEventHandler(){
         this.element.classList.toggle('food--active'); //指定したクラスがあった場合はクラスを付け足す。
-        const score = new Score();
+        const score = Score.getInstance();
         score.render();
     }
 }
 //Foodクラスの一覧
-class Foods{
+class Foods implements Foodsable{
+    //シングルトンパターン->毎回インスタンスを生成させない
+    private static instance:Foods;
     elements = document.querySelectorAll<HTMLDivElement>('.food'); //クラスがfoodの要素を全て取得する。ジェネリクスで要素を指定できる。
     private _activeElements:HTMLDivElement[] = [];
     private _activeElementsScore:number[] = [];
@@ -48,12 +75,19 @@ class Foods{
         })
         return this._activeElementsScore;
     }
-    //各要素を取得する。
-    constructor(){
+    //各要素を取得する。newをクラス内でしか使用できなくする。
+    private constructor(){
         this.elements.forEach(element => {
             new Food(element); //各Foodに関する詳細な処理はFoodクラスで実行する。
         })
     }
+    //インスタンスの取得
+    static getInstance(){
+        if(!Foods.instance){
+            Foods.instance = new Foods();
+        }
+        return Foods.instance;
+    }
 }
 
-const foods = new Foods();
+const foods = Foods.getInstance();
